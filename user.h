aa28586c846cec7a0ee254a8be935202ba5b44aa
Gus Sandoval <gussand@gmail.com>
2020-05-17 17:42:12 -0400

    code for final

--- user.h
+++ user.h
@@ -23,6 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+uint random(void);
 
 // ulib.c
 int stat(char*, struct stat*);
@@ -32,7 +33,7 @@ char* strchr(const char*, char c);
 int strcmp(const char*, const char*);
 void printf(int, char*, ...);
 char* gets(char*, int max);
-uint strlen(char*);
+uint strlen(const char*);
 void* memset(void*, int, uint);
 void* malloc(uint);
 void free(void*);
